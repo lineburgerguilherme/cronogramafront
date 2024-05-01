abaaf48998fa52a98e25086db323dc09d4b58ae0
Guilherme Machado Lineburger <103149953+lineburgerguilherme@users.noreply.github.com>
2024-05-01 15:26:13 -0300

    router
    Co-Authored-By: Felipe Oliveira de Souza <49538342+f3l8p4@users.noreply.github.com>

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import { Router,Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import CadDisciplina from '../components/CadastroDisciplinas';
 import CadProfessor from '../components/CadastroProfessores';
 import CadastroSala from '../components/CadastroSala';
-import CadSala from '../components/CadastroSala';
 import CadFase from '../components/CadastroFase';
 import Login from '../components/Login';
 import CadCurso from '../components/Curso';
-import CadastroDiaExcecao from '../components/CadastroDiaExeção';
-
+import CadastroDiaExcecao from '../components/CadastroDiaExcecao'; 
 
 const Routes = () => {
   return (
     <Router>
-            <Route path='/disciplina' component={<CadDisciplina />} />
-            <Route path='/professor' componet={<CadProfessor />} />
-            <Route path='/sala' conponet ={<CadSala />}/>
-            <Route path='/fase' conponet = {<CadFase />}/> 
-            <Route path='/login' conponet = {<Login />}/>
-            <Route path='/curso' componet = {<CadCurso />}/>
-            <Route path='/Execao' componet = {<CadastroDiaExcecao />}/>
+      <Route path='/disciplina' component={CadDisciplina} />
+      <Route path='/professor' component={CadProfessor} />
+      <Route path='/sala' component={CadastroSala} />
+      <Route path='/fase' component={CadFase} />
+      <Route path='/login' component={Login} />
+      <Route path='/curso' component={CadCurso} />
+      <Route path='/execao' component={CadastroDiaExcecao} /> 
     </Router>
   );
 }
